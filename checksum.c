3c647b8bddfbbe7ff8da2b1d16a6a5a976163eec
Paul (Buero) <paul.zhm@mentana.de>
2017-02-08 14:12:16 +0100

    Programm um Timer zu Programmieren

---
+++ checksum.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	unsigned int on = 730; // HHMM
+	unsigned int off = 1630; // HHMM
+	unsigned int dc = 255;
+	unsigned int multi = 35;
+		
+	unsigned char ck_a = 0;
+	unsigned char ck_b = 0;
+
+	unsigned char payload[] = {on>>8, on, (off>>8), off, dc, multi};
+
+	printf("%02X", 0xB9);
+	
+	unsigned int i;
+	for(i = 0; i < 6; i++)
+	{
+		ck_a = ck_a + payload[i];
+		ck_b = ck_b + ck_a;
+		printf("%02X", payload[i]);
+	}
+	
+	printf("%02X%02X", ck_a, ck_b);
+	
+	return 0;
+}
